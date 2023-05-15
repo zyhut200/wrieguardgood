67dff09d791e7ef93edf08adc79b11025edb708b
zyhut200 <99744043+zyhut200@users.noreply.github.com>
2023-05-16 04:48:41 +0800

    Update wggoog.sh

--- wggoog.sh
+++ wggoog.sh
@@ -6,7 +6,7 @@ wget https://raw.githubusercontent.com/zyhut200/wrieguardgood/main/wg_mult6.sh
 wget https://raw.githubusercontent.com/zyhut200/wrieguardgood/main/wg_mult7.sh
 chmod +x ./wg_mult2.sh && chmod +x ./wg_mult3.sh && chmod +x ./wg_mult4.sh && chmod +x ./wg_mult5.sh && chmod +x ./wg_mult6.sh && chmod +x ./wg_mult7.sh && ./wg_mult2.sh && ./wg_mult3.sh && ./wg_mult4.sh && ./wg_mult5.sh && ./wg_mult6.sh && ./wg_mult7.sh
 mkdir ./$(curl -s ifconfig.me)
-mv /etc/wireguard/001.conf $(curl -s ifconfig.me) && mv /etc/wireguard/001.conf /etc/wireguard/002.conf  /etc/wireguard/003.conf  /etc/wireguard/004.conf  /etc/wireguard/005.conf $(curl -s ifconfig.me) 
+mv /etc/wireguard/001.conf $(curl -s ifconfig.me) && mv /etc/wireguard/001.conf /etc/wireguard/002.conf /etc/wireguard/003.conf /etc/wireguard/004.conf /etc/wireguard/005.conf $(curl -s ifconfig.me) 
 tar -cvf $(curl -s ifconfig.me).tar $(curl -s ifconfig.me)
 yum&apt -y install lrzsz
 sz ./$(curl -s ifconfig.me).tar

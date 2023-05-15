454b806944aaffc6be4ca2b3381f2dbee660e5bd
zyhut200 <99744043+zyhut200@users.noreply.github.com>
2023-05-16 04:25:41 +0800

    Update wggoog.sh

--- wggoog.sh
+++ wggoog.sh
@@ -7,4 +7,4 @@ wget https://raw.githubusercontent.com/zyhut200/wrieguardgood/main/wg_mult7.sh
 chmod +x ./wg_mult2.sh && chmod +x ./wg_mult3.sh && chmod +x ./wg_mult4.sh && chmod +x ./wg_mult5.sh && chmod +x ./wg_mult6.sh && chmod +x ./wg_mult7.sh && ./wg_mult2.sh && ./wg_mult3.sh && ./wg_mult4.sh && ./wg_mult5.sh && ./wg_mult6.sh && ./wg_mult7.sh
 tar -cvf $(curl -s ifconfig.me).tar $(curl -s ifconfig.me)
 yum&apt -y install lrzsz
-sz ./54.168.242.28.tar
+sz ./$(curl -s ifconfig.me).tar

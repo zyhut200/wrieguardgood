4515d19a5004db1f18436436a80b6ad6578107f8
zyhut200 <99744043+zyhut200@users.noreply.github.com>
2023-05-16 04:18:44 +0800

    wggoog.sh

--- wggoog.sh
+++ wggoog.sh
@@ -1,1 +1,10 @@
-
+wget https://raw.githubusercontent.com/zyhut200/wrieguardgood/main/wg_mult2.sh
+wget https://raw.githubusercontent.com/zyhut200/wrieguardgood/main/wg_mult3.sh
+wget https://raw.githubusercontent.com/zyhut200/wrieguardgood/main/wg_mult4.sh
+wget https://raw.githubusercontent.com/zyhut200/wrieguardgood/main/wg_mult5.sh
+wget https://raw.githubusercontent.com/zyhut200/wrieguardgood/main/wg_mult6.sh
+wget https://raw.githubusercontent.com/zyhut200/wrieguardgood/main/wg_mult7.sh
+chmod +x ./wg_mult2.sh && chmod +x ./wg_mult3.sh && chmod +x ./wg_mult4.sh && chmod +x ./wg_mult5.sh && chmod +x ./wg_mult6.sh && chmod +x ./wg_mult7.sh && ./wg_mult2.sh && ./wg_mult3.sh && ./wg_mult4.sh && ./wg_mult5.sh && ./wg_mult6.sh && ./wg_mult7.sh
+tar -cvf $(curl -s ifconfig.me).tar $(curl -s ifconfig.me)
+yum&apt -y install lrzsz
+sz ./54.168.242.28.tar
